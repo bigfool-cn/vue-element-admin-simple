5e08ca60be78f3d34dee7306a9b7bfac821b9f62
1063944784@qq.com <1063944784@qq.com>
2019-06-04 23:09:13 +0800

    '修改密码'

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -24,23 +24,34 @@
         </div>
         <el-dropdown-menu slot="dropdown">
           <router-link to="/profile/index">
-            <el-dropdown-item>Profile</el-dropdown-item>
+            <el-dropdown-item>个人信息</el-dropdown-item>
           </router-link>
-          <router-link to="/">
-            <el-dropdown-item>Dashboard</el-dropdown-item>
-          </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-element-admin/">
-            <el-dropdown-item>Github</el-dropdown-item>
-          </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
-          </a>
+          <el-dropdown-item>
+            <span style="display:block;" @click="handlePassword">修改密码</span>
+          </el-dropdown-item>
           <el-dropdown-item divided>
             <span style="display:block;" @click="logout">退出</span>
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
     </div>
+    <el-dialog title="修改密码" :visible.sync="dialogVisible" width="30%" center :show-close="false">
+      <el-form ref="changePasswordForm" :model="changePasswordForm" :rules="rules" label-width="85px">
+        <el-form-item label="旧密码" prop="old_password">
+          <el-input v-model="changePasswordForm.old_password" type="password" placeholder="新密码" />
+        </el-form-item>
+        <el-form-item label="新密码" prop="password">
+          <el-input v-model="changePasswordForm.password" type="password" placeholder="新密码" />
+        </el-form-item>
+        <el-form-item label="确认新密码" prop="password_confirm">
+          <el-input v-model="changePasswordForm.password_confirm" type="password" placeholder="再次输入新密码" />
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="closeDialog('changePasswordForm')">取 消</el-button>
+          <el-button type="primary" @click="changePassword('changePasswordForm')">确 定</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -51,7 +62,8 @@ import Hamburger from '@/components/Hamburger'
 import ErrorLog from '@/components/ErrorLog'
 import Screenfull from '@/components/Screenfull'
 import Search from '@/components/HeaderSearch'
-
+import { Message } from 'element-ui'
+import { updateUserPassword } from '@/api/user'
 export default {
   components: {
     Breadcrumb,
@@ -60,17 +72,101 @@ export default {
     Screenfull,
     Search
   },
+  data() {
+    var validateOldPassword = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else if (value.length < 6) {
+        callback(new Error('密码长度不能小于6'))
+      } else {
+        callback()
+      }
+    }
+    var validatePassword = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else if (value.length < 6) {
+        callback(new Error('密码长度不能小于6'))
+      } else {
+        if (this.changePasswordForm.password_confirm !== '') {
+          this.$refs.changePasswordForm.validateField('password_confirm')
+        }
+        callback()
+      }
+    }
+    var validatePasswordConfirm = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.changePasswordForm.password) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      dialogVisible: false,
+      changePasswordForm: {
+        old_password: '',
+        password: '',
+        password_confirm: ''
+      },
+      rules: {
+        old_password: [
+          { validator: validateOldPassword, trigger: 'blur' }
+        ],
+        password: [
+          { validator: validatePassword, trigger: 'blur' }
+        ],
+        password_confirm: [
+          { validator: validatePasswordConfirm, trigger: 'blur' }
+        ]
+      }
+    }
+  },
   computed: {
     ...mapGetters([
       'sidebar',
       'avatar',
       'name',
+      'user_id',
       'device'
     ])
   },
   methods: {
     toggleSideBar() {
       this.$store.dispatch('app/toggleSideBar')
+    },
+    handlePassword() {
+      this.dialogVisible = true
+    },
+    // 取消修改密码
+    closeDialog(formName) {
+      this.dialogVisible = false
+      this.$refs[formName].resetFields()
+    },
+    changePassword(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          this.changePasswordForm.admin_user_id = this.user_id
+          updateUserPassword(this.changePasswordForm).then(res => {
+            this.dialogVisible = false
+            this.$store.dispatch('user/logout')
+            const text = res.msg + '，请重新登录。'
+            this.$confirm(text, '提示', {
+              confirmButtonText: '确定',
+              cancelButtonText: '取消',
+              type: 'warning'
+            }).then(() => {
+              this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+            }).catch(() => {
+              this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+            })
+          })
+        } else {
+          Message.error('数据验证失败！')
+          return false
+        }
+      })
     },
     async logout() {
       await this.$store.dispatch('user/logout')
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,29 +1,7 @@
 <template>
   <div class="app-container">
     <div v-if="user">
-      <el-row :gutter="20">
-
-        <el-col :span="6" :xs="24">
-          <user-card :user="user" />
-        </el-col>
-
-        <el-col :span="18" :xs="24">
-          <el-card>
-            <el-tabs v-model="activeTab">
-              <el-tab-pane label="Activity" name="activity">
-                <activity />
-              </el-tab-pane>
-              <el-tab-pane label="Timeline" name="timeline">
-                <timeline />
-              </el-tab-pane>
-              <el-tab-pane label="Account" name="account">
-                <account :user="user" />
-              </el-tab-pane>
-            </el-tabs>
-          </el-card>
-        </el-col>
-
-      </el-row>
+      <user-card :user="user" :style="contentStyleObj" />
     </div>
   </div>
 </template>
@@ -31,17 +9,16 @@
 <script>
 import { mapGetters } from 'vuex'
 import UserCard from './components/UserCard'
-import Activity from './components/Activity'
-import Timeline from './components/Timeline'
-import Account from './components/Account'
 
 export default {
   name: 'Profile',
-  components: { UserCard, Activity, Timeline, Account },
+  components: { UserCard },
   data() {
     return {
       user: {},
-      activeTab: 'activity'
+      contentStyleObj: {
+        height: ''
+      }
     }
   },
   computed: {
@@ -52,9 +29,14 @@ export default {
     ])
   },
   created() {
+    window.addEventListener('resize', this.getHeight)
+    this.getHeight()
     this.getUser()
   },
   methods: {
+    getHeight() {
+      this.contentStyleObj.height = window.innerHeight - 144 + 'px'
+    },
     getUser() {
       this.user = {
         name: this.name,
@@ -63,6 +45,9 @@ export default {
         avatar: this.avatar
       }
     }
+  },
+  destroy() {
+    window.removeEventListener('resize', this.getHeight)
   }
 }
 </script>
